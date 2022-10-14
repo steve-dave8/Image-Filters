bd1baaadf61bfa70728485d750259184b063babc
steve-dave8 <steve-dave8@users.noreply.github.com>
2022-10-14 23:35:08 +0000

    deploy: 13d2987092f156b41348c01a816f3bd5381c1137

--- assets/filter-functions/noise&specks.js
+++ assets/filter-functions/noise&specks.js
@@ -183,16 +183,58 @@ const retroviolet = (imgData) => {
     return imgData;
 };
 
-const white_noise = (imgData, max) => {
+/**
+ * @param {(string|Object)} color - string representing hexadecimal color value OR object containing RGB color values in the format {R: number; G: number; B: number}
+ * @param {number} max - influences frequency that colored specks will appear; minimum value of 12
+ */
+const custom_noise = (imgData, color, max) => {
+    let R, G, B;
+    if (typeof color === "object") {
+        if (color.hasOwnProperty("R") && color.hasOwnProperty("G") && color.hasOwnProperty("B")) {
+            R = color.R;
+            G = color.G;
+            B = color.B;
+        }
+    } else if (typeof color === "string") {
+        // Convert hexadecimal color code to rgb values:
+        let hdColor = color.replace("#", "").trim();
+        if (hdColor.length === 3) {
+            const shorthandRegex = /([a-f\d])([a-f\d])([a-f\d])$/i;
+            hdColor = hdColor.replace(shorthandRegex, function(m, r, g, b) {
+                return r + r + g + g + b + b;
+            });
+        }
+        if (hdColor.length === 6) {
+            const rgbResult = /([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hdColor);
+            if (rgbResult) {
+                R = parseInt(rgbResult[1], 16);
+                G = parseInt(rgbResult[2], 16);
+                B = parseInt(rgbResult[3], 16);
+            }
+        }
+    }
+
+    if (R === undefined || G === undefined || B === undefined) return;
+
     for (let i = 0; i < imgData.data.length; i += 4) {
         let randomNumber = getRandomNumber(0, 100);
         if (randomNumber > 10 && randomNumber < max) {
-            imgData.data[i] = 255;
-            imgData.data[i + 1] = 255;
-            imgData.data[i + 2] = 255;
+            imgData.data[i] = R;
+            imgData.data[i + 1] = G;
+            imgData.data[i + 2] = B;
         }
     }
     return imgData;
+};
+
+const black_noise = (imgData, max) => {
+    const black = {R: 0, G: 0, B: 0};
+    return custom_noise(imgData, black, max);
+};
+
+const white_noise = (imgData, max) => {
+    const white = {R: 255, G: 255, B: 255};
+    return custom_noise(imgData, white, max);
 };
 
 const confetti_imgdata = (imgData) => {
@@ -222,5 +264,7 @@ export {
     cosmic_imgdata,
     retroviolet,
     white_noise,
-    confetti_imgdata
+    confetti_imgdata,
+    black_noise,
+    custom_noise
 }
